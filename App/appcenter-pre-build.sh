accb5709af665164b57935fb9dfc195d0f93b6ce
Mark Lee <mlee@secc.org>
2022-01-12 16:51:22 -0500

    Added debug code to solve build issue

--- App/appcenter-pre-build.sh
+++ App/appcenter-pre-build.sh
@@ -1,4 +1,4 @@
 echo "Updating ServiceBus connection string to $breweventConnectionString"
 sed -i -e "s/breweventConnectionString/$breweventConnectionString/g"  $APPCENTER_SOURCE_DIRECTORY/App/BrewMaster/Messaging/ServiceBusManager.cs
 
-printf '%b\n' "$($APPCENTER_SOURCE_DIRECTORY/App/BrewMaster/Messaging/ServiceBusManager.cs)"
+echo "$($APPCENTER_SOURCE_DIRECTORY/App/BrewMaster/Messaging/ServiceBusManager.cs)"
