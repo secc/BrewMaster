6a9d5de220eac6b0bdfb72a16d92fc582dbca652
Mark Lee <mlee@secc.org>
2022-01-12 15:41:57 -0500

    Fix pre-build script

--- App/appcenter-pre-build.sh
+++ App/appcenter-pre-build.sh
@@ -1,1 +1,2 @@
-sed -i -e "s/breweventConnectionString/$breweventConnectionString/g""  $APPCENTER_SOURCE_DIRECTORY/BrewMaster/Messaging/ServiceBusManger.cs
+echo "Updating ServiceBus connection string to $breweventConnectionString"
+sed -i -e "s/breweventConnectionString/$breweventConnectionString/g"  $APPCENTER_SOURCE_DIRECTORY/BrewMaster/Messaging/ServiceBusManger.cs
