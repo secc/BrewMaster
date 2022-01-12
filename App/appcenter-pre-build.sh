6fbf1eb5b9161b1824d3945678befe5d07d0cc5b
Mark Lee <mlee@secc.org>
2022-01-12 16:09:21 -0500

    Fixed typo

--- App/appcenter-pre-build.sh
+++ App/appcenter-pre-build.sh
@@ -1,2 +1,2 @@
 echo "Updating ServiceBus connection string to $breweventConnectionString"
-sed -i -e "s/breweventConnectionString/$breweventConnectionString/g"  $APPCENTER_SOURCE_DIRECTORY/App/BrewMaster/Messaging/ServiceBusManger.cs
+sed -i -e "s/breweventConnectionString/$breweventConnectionString/g"  $APPCENTER_SOURCE_DIRECTORY/App/BrewMaster/Messaging/ServiceBusManager.cs
