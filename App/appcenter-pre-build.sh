d988ff193b88ed653aef97ee46b5abfff32e9eb2
Mark Lee <mlee@secc.org>
2022-01-13 10:51:30 -0500

    Added Bash Header

--- App/appcenter-pre-build.sh
+++ App/appcenter-pre-build.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 echo "Updating ServiceBus connection string to $breweventConnectionString"
 sed -i -e "s/breweventConnectionString/$breweventConnectionString/g"  $APPCENTER_SOURCE_DIRECTORY/App/BrewMaster/Messaging/ServiceBusManager.cs
 
