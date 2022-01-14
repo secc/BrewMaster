a3e92bb6af1c298c1c889ea504741d004cb9306b
Mark Lee <mlee@secc.org>
2022-01-14 16:15:21 -0500

    Build debug tests

--- App/appcenter-pre-build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Writing config file"
-
-echo "{\"ServiceBusConnectionString\":\"$breweventConnectionString\"}"  > $APPCENTER_SOURCE_DIRECTORY/App/BrewMaster.iOS/config.json
